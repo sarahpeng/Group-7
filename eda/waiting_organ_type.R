ee023e4fb81a98747aa3b029dd5e0d02cd0754ba
Lufei Wang <lufeiw@uw.edu>
2020-05-18 10:46:45 -0700

    lufei and ming's

---
+++ eda/waiting_organ_type.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(tidyr)
+organ_waiting <- read.csv(file = 'Organ_by_Blood_Type.csv', stringsAsFactors = FALSE)
+organ_waiting_c <- subset(organ_waiting, select = -c(X.1, All.Organs))
+organ_waiting_c = organ_waiting_c[-1,1:6]
+waiting_long <- gather(organ_waiting_c, Organ, Waiting_Num, 2:6, factor_key=TRUE)
+waiting_long$Waiting_Num <- as.numeric(gsub(",", "", waiting_long$Waiting_Num))
+ggplot(data=waiting_long, aes(x=Organ, y=Waiting_Num, fill=X)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  xlab("Organ Type") + 
+  ylab("Number of people waiting") + 
+  ggtitle("Organ Type vs Number of people waiting") 
