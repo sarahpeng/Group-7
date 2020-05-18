99e1baa4557e65ee12eef64218a84fadb7b9978f
scuj <scuj@uw.edu>
2020-05-17 19:19:15 -0700

    updated to more comprehensive graph and filtered data

--- eda/livingvsdeceaseddistribution.R
+++ eda/livingvsdeceaseddistribution.R
@@ -6,10 +6,18 @@ living_vs_deceased <- read.csv(file = 'distributiondata.csv')
 
 living_vs_deceased <- living_vs_deceased %>% 
   rename(
-    Donors = X
+    Donors = X,
+    Genders = X.1,
+    Ages = X.2
   )
 
-plot1 <-ggplot(data=living_vs_deceased, aes(x=Donors, y=To.Date)) + 
+all_ages <- living_vs_deceased %>%
+  select(Donors, Genders, Ages, To.Date)%>%
+  filter(Ages== ("All Ages"),
+         Genders== ("All Genders")
+         )
+
+plot1 <-ggplot(data=all_ages, aes(x=Donors, y=To.Date)) + 
   xlab("Types of Donors") + ylab("Number of Donors") + ggtitle("Living vs. Deceased Donors") +
   geom_bar(stat="identity", fill="steelblue")
 
