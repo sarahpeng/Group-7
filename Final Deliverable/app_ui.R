8b9cec1715b7abe6c6e90c2f49d082feec8988b4
Sarah Peng <Sarah@sarahpesmacbook.home>
2020-06-08 12:23:27 -0700

    update

--- Final Deliverable/app_ui.R
+++ Final Deliverable/app_ui.R
@@ -6,8 +6,9 @@ waiting_time <- read.csv("Organ_by_Blood_Type.csv", stringsAsFactors = FALSE)
 library(shiny)
 page_one <- tabPanel(
   "Introduction",
-  tags$h1("Why this subject?"),
-  p("Organ donation is an important part of the health care system which has not received enough attention. Many of the people only have heard about it, however, they either have not signed up to
+  h1(strong("Introduction")),img(src='logo1.jpg', align = "right"),
+  tags$h1("Objection"),
+  p("Organ donation is an important part of the health care system which has not received enough attention. Many people only have heard about it, however, they either have not signed up to
     be donors nor have them realized the fact that there are lives saved by receiving the right organ. We want to compare the attitudes towards organ donations and transplants to the actual donation
     statistics to see if the correlation between support and action. And probably make more people participate in the system."),
   tags$h1("What we have:"),
@@ -15,7 +16,7 @@ page_one <- tabPanel(
     The trend of supply tells the preference of donors when it comes to organ donation. The waiting list graph visualized the demand of organs differentiated by blood types and age. Combining both graphs,
     users will have findings including but not limited to which organ is most wanted and which organ has more supplies than demands. As for the table, latest information on how general population view organ
     donation is shown. The attitudes and actions are compared."),
-  tags$h1("Soruces:"),
+  tags$h1("Sources:"),
   p("Data used is mainly from [Organ Donation Statistics](https://www.organdonor.gov/statistics-stories/statistics.html)"),
   p("The survey is from National Survey of Organ Donation Attitudes and Practices, 2019](https://www.organdonor.gov/sites/default/files/about-dot/files/nsodap-organ-donation-survey-2019.pdf)"),
   tags$h1("Contributors:"),
@@ -48,6 +49,29 @@ page_two <- tabPanel(
   br(),
   br(),
   plotOutput("piechart_plot"),
+  tags$h4("Donors"),
+  p("In this page, the graph shows the blood type of either the living or 
+  deceased donor organ from 1988 to 2019. We did not include the data from 
+  2020 because it was incomplete. From the graph, we see that theorgans that 
+  were the least donated from living donors were the heart, pancreas, and 
+  intestine. The most popular blood type of the organs was AB and A. The 
+  most donated organ from a living donor was kidneys. This may be because 
+  humans have two kidneys and can live off of one, thus there is a higher 
+  possibility for a living donor to donate their kidney. The liver was the 
+  second most donated organ which could be due to the fact that people 
+  can live with half a live because it regenerates cells on its own to become 
+  whole again in a matter of months. For the heart, pancreas and intestine, 
+  they are all important organs for the body and crucial to live a healthy life 
+  so that may be why there is less donations in those three categories.
+From the deceased donors, all of the organs had a higher number of donations 
+than the living donors. From all the displayed organs, the most popular organ 
+blood type donated was AB and the least popular was type B. The least donated 
+organ from deceased donors was the intestine. This could be because there is the 
+least need for intestine donations. The kidney, liver, and heart donations 
+increased immensely which could be due to the fact that heart disease, kidney
+disease, and cancers are some of the top ten leading deaths in the U.S. 
+Overall, from the pie graph, we see that there is a 16% higher chance of organ 
+    donations coming from deceased donors than living donors from 1988 up to 2020."),
   tags$h4("Note:"),
   p("The pie chart shows the proportion of the two types of donors. As you can see, 
     there is more deceased donors than living donors. The interactive part shows users
@@ -82,32 +106,9 @@ page_three <- tabPanel(
   plotOutput("waiting_plot")
 )
 page_four <- tabPanel(
-  "Interpretation",
-  tags$h4("Donors"),
-  p("In this page, the graph shows the blood type of either the living or 
-  deceased donor organ from 1988 to 2019. We did not include the data from 
-  2020 because it was incomplete. From the graph, we see that theorgans that 
-  were the least donated from living donors were the heart, pancreas, and 
-  intestine. The most popular blood type of the organs was AB and A. The 
-  most donated organ from a living donor was kidneys. This may be because 
-  humans have two kidneys and can live off of one, thus there is a higher 
-  possibility for a living donor to donate their kidney. The liver was the 
-  second most donated organ which could be due to the fact that people 
-  can live with half a live because it regenerates cells on its own to become 
-  whole again in a matter of months. For the heart, pancreas and intestine, 
-  they are all important organs for the body and crucial to live a healthy life 
-  so that may be why there is less donations in those three categories.
-From the deceased donors, all of the organs had a higher number of donations 
-than the living donors. From all the displayed organs, the most popular organ 
-blood type donated was AB and the least popular was type B. The least donated 
-organ from deceased donors was the intestine. This could be because there is the 
-least need for intestine donations. The kidney, liver, and heart donations 
-increased immensely which could be due to the fact that heart disease, kidney
-disease, and cancers are some of the top ten leading deaths in the U.S. 
-Overall, from the pie graph, we see that there is a 16% higher chance of organ 
-    donations coming from deceased donors than living donors from 1988 up to 2020."
+  "Interpretation"
   
-))
+)
 
 page_five <- tabPanel("Survey",tags$h4("Background"),
                       p("The 2019 National Survey of Organ Donation Attitudes and 
@@ -130,10 +131,10 @@ page_five <- tabPanel("Survey",tags$h4("Background"),
                         strongly support, support oppose, or strongly oppose the donation of organs for transplants,
                         people responded"), p("When asked if you would want your organs donated after death, people responded"),
                       p("And when asked whether you beleive the following statements about organ donation are true, people responded"), 
-                      tags$h4("Analysis"), plotOutput("waitlist_plot"))
+                      tags$h4("Analysis"))
 
 ui <- navbarPage(
-  "My Application", 
+  "Organ Donation", 
   page_one,         
   page_two,         
   page_three,
