9a3f42574f1140aa362a959d0d151c7ac6e0126d
scuj <scuj@uw.edu>
2020-06-08 09:13:23 -0700

    graphs complete, just need to rename and color

--- Final Deliverable/survey.R
+++ Final Deliverable/survey.R
@@ -9,21 +9,30 @@ sentences <- read.csv('data/sentences.csv')
 kable(population, col.names = c("Total", "", "10,000"))
 
 #pie
-graphs%>% filter(Question == "SignedUp") %>% select(Response,Percent)
-graphs%>% filter(Question == "Willing") %>% select(Response,Percent)
+signedup <- graphs%>% filter(Question == "SignedUp") %>% select(Response, Percent)
+willing <- graphs%>%filter(Question == "Willing") %>% select(Response, Percent)
 
+kable(signedup)
+kable(willing)
 
 #bar
+waitlistDie <- graphs%>% filter(Question == "WaitlistDie") %>% select(Response,Percent)
+ggplot(waitlistDie) + geom_col(mapping = aes(x = reorder(waitlistDie$Response, -waitlistDie$Percent) , y = waitlistDie$Percent ))
 
-graphs%>% filter(Question == "WaitlistDie") %>% select(Response,Percent)
 
-graphs%>% filter(Question == "TransplantSupport") %>% select(Response,Percent)
+transplantSupport <- graphs %>% filter(Question == "TransplantSupport")  %>% select(Response,Percent)
+ggplot(transplantSupport) + geom_col(mapping = aes(x = reorder(transplantSupport$Response, -transplantSupport$Percent) , y =transplantSupport$Percent ))
 
-graphs%>% filter(Question == "DeathDonate") %>% select(Response,Percent)
+deathDonate <- graphs%>% filter(Question == "DeathDonate") %>% select(Response,Percent)
+ggplot(deathDonate) + geom_col(mapping = aes(x = reorder(deathDonate$Response, -deathDonate$Percent) , y = deathDonate$Percent ))
 
 # stacked bar
-graphs%>% filter(Question == "Kidney") %>% select(Response,Percent)
+kidney <- graphs%>% filter(Question == "Kidney") %>% select(Response,Percent)
+ggplot(kidney) + geom_col(mapping = aes(x = reorder(kidney$Response, -kidney$Percent) , y = kidney$Percent ))
 
-graphs%>% filter(Question == "Liver") %>% select(Response,Percent)
+liver <- graphs%>% filter(Question == "Liver") %>% select(Response,Percent)
+ggplot(liver) + geom_col(mapping = aes(x = reorder(liver$Response, -liver$Percent) , y = liver$Percent ))
 
-graphs%>% filter(Question == "Lung") %>% select(Response,Percent)
+lung <- graphs%>% filter(Question == "Lung") %>% select(Response,Percent)
+ggplot(lung) + geom_col(mapping = aes(x = reorder(lung$Response, -lung$Percent) , y = lung$Percent ))
+
